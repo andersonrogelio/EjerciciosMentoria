3d6f00c218cc90c63869603a095400ff546460c3
Anderson <andersonrogelio0@gmail.com>
2022-06-29 12:29:24 -0300

    Cree la clase ConversorMillasaKm.java que nos convierte millas en km hasta que introduzcamos la cantidad de 0 millas

--- src/main/java/entregable/ConversorMillasaKm.java
+++ src/main/java/entregable/ConversorMillasaKm.java
@@ -1,4 +1,24 @@
 package entregable;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class ConversorMillasaKm {
+
+    public static void main(String[] args) {
+        int millas;
+        double km;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingrese las millas");
+        millas = s.nextInt();
+        km = millas *1.6093;
+        DecimalFormat formato = new DecimalFormat("#.##");
+        System.out.println("Esta cantidad de millas "+millas+ " equivale a "+formato.format(km)+" en km");
+        while (millas != 0){
+            System.out.println("Ingrese las millas");
+            millas = s.nextInt();
+            km = millas *1.6093;
+            System.out.println("Esta cantidad de millas "+millas+ " equivale a "+formato.format(km)+" en km");
+        }
+    }
 }
